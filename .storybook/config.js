0124f9eb8fcbf48641eabe42a6a83566fa5980c2
Rob LaFeve <roblafeve@gmail.com>
2019-09-21 21:09:37 -0500

    Update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,6 @@
 import { configure } from "@storybook/react"
 import "loki/configure-react"
 
-const files = require.context("../src", true, /stories\.tsx$/)
+const context = require.context("../src", true, /stories\.tsx$/)
 
-function loadStories() {
-  const filenames = files.keys()
-  filenames.length ? filenames.forEach(req) : require("./storyBase.js")
-}
-
-configure(loadStories, module)
+configure(context.keys().length ? context : () => require("./storyBase.js"), module)
